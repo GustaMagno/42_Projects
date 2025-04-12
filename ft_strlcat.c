68995a6f28c2be414117a0be324df4fcf07f4891
Gustavo <gumagno2005@gmail.com>
2025-04-12 01:05:54 +0100

    att funcoes

---
+++ ft_strlcat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/12 00:19:21 by marvin            #+#    #+#             */
+/*   Updated: 2025/04/12 00:19:21 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = ft_strlen(dest);
+	j = 0;
+	if (size == 0)
+		return (ft_strlen(src) + i);
+	while (src[j] && (i + j) < size - 1)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (i + ft_strlen(src));
+}
