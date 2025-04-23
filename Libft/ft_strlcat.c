735d20cd398d6598f0aae08c8c1f22a3f79bbfbd
Gustavo Magno Oliveira Calmon De Siqueira <gustoliv@c2r8s10.42lisboa.com>
2025-04-23 19:19:22 +0100

    ultima parada

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gustoliv <gustoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 12:03:29 by gustoliv          #+#    #+#             */
-/*   Updated: 2025/04/23 14:16:19 by gustoliv         ###   ########.fr       */
+/*   Updated: 2025/04/23 19:18:35 by gustoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t	src_len;
 	size_t	dest_len;
 
-	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
+	dest_len = ft_strlen(dest);
 	i = 0;
 	if (dest_len >= size)
 		return (size + src_len);
